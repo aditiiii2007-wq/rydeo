ca9b19b5333b3dd133e43231db79fc931dc09b5f
aditiiii2007-wq <aditiiii2007@gmail.com>
2026-05-20 23:21:32 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    base: '/rydeo/',
+    base: './',
     plugins: [react()],
     build: {
       outDir: 'docs',
